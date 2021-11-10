86b7fad0027b3f68cdd8351c6842145cdfb9f272
AxelDemorest <axel.demorest@gmail.com>
2021-11-10 15:53:53 +0100

    Session fixed

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -9,13 +9,19 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController{
-    public function index(PostRepository $postRepository): Response
+    public function index(PostRepository $postRepository, SessionInterface $session): Response
     {
-        return $this->render('index.html.twig', [
-            'posts' => $postRepository->findAll(),
-        ]);
+        if(!$session->has("login")) {
+            return $this->redirectToRoute('user_connexion', [], Response::HTTP_SEE_OTHER);
+        } else {
+            return $this->render('index.html.twig', [
+                'posts' => $postRepository->findAll(),
+            ]);
+        }
+
     }
 }
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -21,11 +21,12 @@ class UserController extends AbstractController
     /**
      * @Route("/", name="user_index", methods={"GET"})
      */
-    public function index(UserRepository $userRepository): Response
+    public function index(UserRepository $userRepository, SessionInterface $session): Response
     {
-        return $this->render('user/index.html.twig', [
-            'users' => $userRepository->findAll(),
-        ]);
+            return $this->render('user/index.html.twig', [
+                'users' => $userRepository->findAll(),
+            ]);
+
     }
 
     /**
@@ -56,7 +57,9 @@ class UserController extends AbstractController
      */
     public function connexion(Request $request, SessionInterface $session): Response
     {
-        $session->remove("login");
+        if($session->has("login")) {
+            return $this->redirectToRoute('index', [], Response::HTTP_SEE_OTHER);
+        }
         $user = new User();
         $form = $this->createForm(UserConnexionType::class, $user);
         $form->handleRequest($request);
@@ -73,8 +76,8 @@ class UserController extends AbstractController
                 ]);
             }
 
-            $session->set("login", $user->getNickname());
-            return $this->redirectToRoute('user_index', [], Response::HTTP_SEE_OTHER);
+            $session->set("login", $user->getId());
+            return $this->redirectToRoute('index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->renderForm('connexion/connexion.html.twig', [
