71bc1b46904d39b63ba269fa1f6dbe4d4e95e8b9
ImMael <legrand.mael2002@gmail.com>
2021-11-10 09:19:10 +0100

    Add index page

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Post;
+use App\Form\PostType;
+use App\Repository\PostRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController{
+    public function index(PostRepository $postRepository){
+        return $this->render('index.html.twig', [
+            'posts' => $postRepository->findAll(),
+        ]);
+    }
+}
