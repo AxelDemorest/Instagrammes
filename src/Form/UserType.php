44ab5abfce4b5110713106c6be663f42b4fc556d
ImMael <legrand.mael2002@gmail.com>
2021-11-10 10:20:02 +0100

    Push new crud

---
+++ src/Form/UserType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('Nickname')
+            ->add('Email')
+            ->add('ProfilePicture')
+            ->add('biography')
+            ->add('post')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
